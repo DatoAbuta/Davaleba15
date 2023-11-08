3e3cc97782b89c7cf81a003cc60d9646341179ab
David69699 <abutidzedato18@gmail.com>
2023-11-09 00:26:21 +0400

    davaleba15

---
+++ main.js
@@ -0,0 +1,13 @@
+function checkEmailValidity() {
+    const emailInput = document.getElementById("email.input");
+    const errorMessage = document.getElementById("erroris-mesiji");
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+  
+    if (!emailPattern.test(emailInput.value)) {
+      errorMessage.innerHTML = "Oops! Please check your email";
+      errorMessage.style.color = 'red'
+    } else {
+      errorMessage.innerHTML = "Your Mail Is Valid";
+      errorMessage.style.color = 'green'
+    }
+  }
